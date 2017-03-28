c0d6f798cb85e41def92687c31bd2f2be6298d77
Jake Larkin <jake8@uber.com>
2017-03-28 12:30:28 -0700

    adds max_pool_size option

--- pool.js
+++ pool.js
@@ -50,12 +50,39 @@ function Pool(http, endpoints, options) {
     this.name = options.name;
     this.options = options;
     this.max_pending = options.max_pending || options.maxPending || 1000;
+    this.max_pool_size = options.max_pool_size || options.maxPoolSize;
     this.endpoints = [];
     this.endpoints_by_name = {};
 
+    // clone
+    this.all_hostports = endpoints.slice();
+
+    if (this.max_pool_size !== undefined && this.max_pool_size !== null &&
+        (typeof this.max_pool_size !== 'number' || this.max_pool_size < 1)) {
+        throw new Error('max_pool_size invalid');
+    }
+
+    var selectedEndpoints = endpoints;
+
+    if (this.max_pool_size < endpoints.length) {
+        // simple resevoir sampling algorithm
+        selectedEndpoints = new Array(this.max_pool_size);
+        for (var j = 0; j < endpoints.length; j++) {
+            if (j < this.max_pool_size) {
+                selectedEndpoints[j] = endpoints[j];
+            } else {
+                // random replacement with decreasing probablity
+                var r = Math.random() * (j + 1);
+                if (r < this.max_pool_size) {
+                    selectedEndpoints[r] = endpoints[j];
+                }
+            }
+        }
+    }
+
     this.length = 0;
-    for (var i = 0; i < endpoints.length; i++) {
-        this.add_endpoint(endpoints[i]);
+    for (var i = 0; i < selectedEndpoints.length; i++) {
+        this.add_pool_endpoint(selectedEndpoints[i]);
     }
 
     if (this.endpoints.length === 0) {
@@ -92,16 +119,16 @@ function Pool(http, endpoints, options) {
 }
 inherits(Pool, EventEmitter);
 
-Pool.prototype.endpoint_health_changed = function (endpoint) {
+Pool.prototype.endpoint_health_changed = function endpoint_health_changed(endpoint) {
     this.emit("health", endpoint.name + " health: " + endpoint.healthy);
 };
 
-Pool.prototype.endpoint_timed_out = function (request) {
+Pool.prototype.endpoint_timed_out = function endpoint_timed_out(request) {
     this.emit("timeout", request);
 };
 
 // returns an array of healthy Endpoints
-Pool.prototype.healthy_endpoints = function () {
+Pool.prototype.healthy_endpoints = function health_endpoints() {
     var healthy = [],
         len = this.endpoints.length;
 
@@ -114,7 +141,7 @@ Pool.prototype.healthy_endpoints = function () {
     return healthy;
 };
 
-Pool.prototype.on_retry = function (err) {
+Pool.prototype.on_retry = function on_retry(err) {
     this.emit("retrying", err);
 };
 
@@ -134,7 +161,7 @@ Pool.prototype.on_retry = function (err) {
 // function(err, res) {}
 
 // for convenience, we allow an option string to be the "path" option
-Pool.prototype.init_req_options = function (options) {
+Pool.prototype.init_req_options = function init_req_options(options) {
     if (! options) {
         return {};
     }
@@ -144,7 +171,7 @@ Pool.prototype.init_req_options = function (options) {
     return options;
 };
 
-Pool.prototype.request = function (options, data, callback) {
+Pool.prototype.request = function request(options, data, callback) {
     var self = this;
 
     options = this.init_req_options(options);
@@ -188,25 +215,25 @@ Pool.prototype.request = function (options, data, callback) {
 
 Pool.prototype.get = Pool.prototype.request;
 
-Pool.prototype.put = function (options, data, callback) {
+Pool.prototype.put = function put(options, data, callback) {
     options = this.init_req_options(options); // note that this will call init_req_options twice, which is fine
     options.method = "PUT";
     return this.request(options, data, callback);
 };
 
-Pool.prototype.post = function (options, data, callback) {
+Pool.prototype.post = function post(options, data, callback) {
     options = this.init_req_options(options);
     options.method = "POST";
     return this.request(options, data, callback);
 };
 
-Pool.prototype.del = function (options, callback) {
+Pool.prototype.del = function del(options, callback) {
     options = this.init_req_options(options);
     options.method = "DELETE";
     return this.request(options, callback);
 };
 
-Pool.prototype.stats = function () {
+Pool.prototype.stats = function stats() {
     var stats = [];
     var len = this.endpoints.length;
     for (var i = 0; i < len; i++) {
@@ -221,7 +248,7 @@ Pool.prototype.stats = function () {
 //   walk through and use the first endpoint we find that is ready()
 //   if none are ready, then check max_pending limits
 //   walk the list of healthy endpoints, returning the first endpoint with below average pending
-Pool.prototype.get_endpoint = function (options) {
+Pool.prototype.get_endpoint = function get_endpoint(options) {
     var endpoints_len = this.endpoints.length;
     var min_pending_level = Infinity, min_pending_endpoint = null;
     var total_pending = 0;
@@ -269,7 +296,7 @@ Pool.prototype.get_endpoint = function (options) {
 Pool.prototype.get_node = Pool.prototype.get_endpoint;
 Pool.prototype.getNode = Pool.prototype.get_endpoint;
 
-Pool.prototype.pending = function () {
+Pool.prototype.pending = function pending() {
     var count = 0;
     var endpoints = this.endpoints;
     for (var i = 0; i < endpoints.length; i++) {
@@ -278,7 +305,7 @@ Pool.prototype.pending = function () {
     return count;
 };
 
-Pool.prototype.rate = function () {
+Pool.prototype.rate = function rate() {
     var count = 0;
     var endpoints = this.endpoints;
     for (var i = 0; i < endpoints.length; i++) {
@@ -287,7 +314,7 @@ Pool.prototype.rate = function () {
     return count;
 };
 
-Pool.prototype.request_count = function () {
+Pool.prototype.request_count = function request_count() {
     var count = 0;
     var endpoints = this.endpoints;
     for (var i = 0; i < endpoints.length; i++) {
@@ -296,32 +323,85 @@ Pool.prototype.request_count = function () {
     return count;
 };
 
-Pool.prototype.close = function () {
+Pool.prototype.close = function close() {
     var endpoints = this.endpoints;
     for (var i = 0; i < endpoints.length; i++) {
         endpoints[i].close();
     }
 };
 
-// Dynamic membership
-Pool.prototype.add_endpoint = function (host_port) {
+Pool.prototype.valid_host_port = function valid_host_port(host_port) {
     var ip_port = host_port.split(":");
     var ip = ip_port[0];
     var port = +ip_port[1];
     if (port > 0 && port < 65536) {
-        var endpoint = new GO.PoolEndpoint(this.http, ip, port, this.options);
-        endpoint.on("health", this.endpoint_health_changed.bind(this));
-        endpoint.on("timeout", this.endpoint_timed_out.bind(this));
-        this.endpoints.push(endpoint);
-        this.length++;
-        this.endpoints_by_name[host_port] = endpoint;
+        return ip_port;
     }
+    return null;
 };
 
-Pool.prototype.remove_endpoint = function (host_port) {
-    var ip_port = host_port.split(":");
+// Dynamic membership
+Pool.prototype.add_endpoint = function add_endpoint(host_port) {
+    var ip_port = this.valid_host_port(host_port);
+    if (!ip_port) {
+        return null;
+    }
+
+    var hasHostPort = this.all_hostports.indexOf(host_port) >= 0;
+    if (!hasHostPort) {
+        this.all_hostports.push(host_port);
+    }
+
+    var maxSizeIsRelevant = this.max_pool_size &&
+        this.max_pool_size < this.all_hostports.length;
+
+    // if using maxSize, probabilistically add the new host port to the
+    // active pool endpoints
+    if (maxSizeIsRelevant) {
+        var r = Math.random() * this.all_hostports.length;
+        // note: the + 1 is a significant bias correction for adding then removing
+        if (r < this.max_pool_size + 1) {
+            this.add_pool_endpoint(host_port);
+            this.adjust_pool_size();
+        }
+        return null;
+    }
+
+    this.add_pool_endpoint(host_port);
+};
+
+Pool.prototype.add_pool_endpoint = function add_pool_endpoint(host_port, ip_port) {
+    ip_port = ip_port || this.valid_host_port(host_port);
+    if (!ip_port) {
+        return;
+    }
+
+    // check already added
+    if (this.endpoints_by_name[host_port]) {
+        return;
+    }
     var ip = ip_port[0];
     var port = +ip_port[1];
+    var endpoint = new GO.PoolEndpoint(this.http, ip, port, this.options);
+    endpoint.on("health", this.endpoint_health_changed.bind(this));
+    endpoint.on("timeout", this.endpoint_timed_out.bind(this));
+    this.endpoints.push(endpoint);
+    this.length++;
+    this.endpoints_by_name[host_port] = endpoint;
+};
+
+Pool.prototype.remove_endpoint = function rm_endpoint(host_port) {
+    for (var j = 0; j < this.all_hostports.length; j++) {
+        if (this.all_hostports[j] === host_port) {
+            this.all_hostports.splice(j, 1);
+        }
+    }
+
+    this.remove_pool_endpoint(host_port);
+    this.adjust_pool_size();
+};
+
+Pool.prototype.remove_pool_endpoint = function rm_pool_endpoint(host_port) {
     var endpoint = this.endpoints_by_name[host_port];
     if (!endpoint) { return; }
 
@@ -337,9 +417,54 @@ Pool.prototype.remove_endpoint = function (host_port) {
     }
 };
 
+// adjusts the pool size within max_pool_size boundary if necessary
+// after we add/remove a discrete element from the pool, this adjustment must
+// carefully select a random element to remove/add so that connections are distributed
+// evenly across the cluster
+Pool.prototype.adjust_pool_size = function adjust_pool_size() {
+    // length can only be out of sync with all hostports if there's a max_pool_size
+    if (!this.max_pool_size) {
+        return;
+    }
+    var expected_size = Math.min(this.max_pool_size, this.all_hostports.length);
+
+    if (expected_size === this.length) {
+        return;
+    }
+
+    while (this.length !== expected_size) {
+        var idx;
+        if (this.length < expected_size) {
+            // add a random host from the all host_ports
+            var unused = this.unused_hostports();
+            idx = Math.floor(Math.random() * unused.length);
+            var randHostPort = unused[idx];
+            if (!this.endpoints_by_name[randHostPort]) {
+                this.add_pool_endpoint(randHostPort);
+            }
+        } else if (this.length > expected_size) {
+            idx = Math.floor(Math.random() * this.length);
+            var randEndpoint = this.endpoints[idx];
+            this.remove_pool_endpoint(randEndpoint.ip + ':' + randEndpoint.port);
+        }
+    }
+};
+
+Pool.prototype.unused_hostports = function unused_hostports() {
+    var unused = new Array(this.all_hostports.length - this.length);
+    var i = 0;
+
+    for (var j = 0; j < this.all_hostports.length; j++) {
+        var hostport = this.all_hostports[j];
+        if (!this.endpoints_by_name[hostport]) {
+            unused[i] = hostport;
+            i++;
+        }
+    }
+    return unused;
+};
 
 module.exports = function init(new_GO) {
     GO = new_GO;
-
-	return Pool;
+    return Pool;
 };
